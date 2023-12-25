2b1cb402828efbda12c798c304f5c9bdf6e0b29f
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-12-25 19:10:29 +0600

    init firebase

--- MyWhatsApp/MyWhatsAppApp.swift
+++ MyWhatsApp/MyWhatsAppApp.swift
@@ -5,10 +5,23 @@
 //  Created by Shah Md Imran Hossain on 25/12/23.
 //
 
+import FirebaseCore
 import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        
+        return true
+    }
+}
 
 @main
 struct MyWhatsAppApp: App {
+    // register app delegate for firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
