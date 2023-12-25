63da04d73715c8d543395119f946b5612410cae4
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-12-25 18:04:07 +0600

    project init

---
+++ MyWhatsApp/MyWhatsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWhatsAppApp.swift
+//  MyWhatsApp
+//
+//  Created by Shah Md Imran Hossain on 25/12/23.
+//
+
+import SwiftUI
+
+@main
+struct MyWhatsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
