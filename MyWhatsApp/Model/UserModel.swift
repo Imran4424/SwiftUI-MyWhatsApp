e17c116c4dc125dae0fb6bc0218cfe23e71e0854
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-12-30 13:26:37 +0600

    authen

---
+++ MyWhatsApp/Model/UserModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserModel.swift
+//  MyWhatsApp
+//
+//  Created by Shah Md Imran Hossain on 30/12/23.
+//
+
+import Foundation
---
+++ MyWhatsApp/View/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  MyWhatsApp
+//
+//  Created by Shah Md Imran Hossain on 30/12/23.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
