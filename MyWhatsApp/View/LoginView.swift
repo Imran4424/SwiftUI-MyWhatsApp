63048ff67941039557ce54ca179fb862b484ca1f
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-12-30 14:22:04 +0600

    logoin view

--- MyWhatsApp/View/LoginView.swift
+++ MyWhatsApp/View/LoginView.swift
@@ -5,13 +5,77 @@
 //  Created by Shah Md Imran Hossain on 30/12/23.
 //
 
+import FirebaseAuth
 import SwiftUI
 
 struct LoginView: View {
+    
+    @State private var email: String = ""
+    @State private var password: String = ""
+    @State private var errorMessage: String = ""
+    
+    private var isFormValid: Bool {
+        if email.isEmptyOrWhiteSpace {
+            return false
+        } else if password.isEmptyOrWhiteSpace {
+            return false
+        }
+        
+        return true
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Form {
+                TextField("Email", text: $email)
+                    .textInputAutocapitalization(.never)
+                SecureField("Password", text: $password)
+                    .textInputAutocapitalization(.never)
+                
+                HStack {
+                    Spacer()
+                    
+                    Button("Log In") {
+                        // take user to login view
+                    }
+                    .disabled(!isFormValid)
+                    .buttonStyle(.borderless)
+                    
+                    Spacer()
+                    
+                    Button("Sign Up") {
+                        Task {
+                            
+                        }
+                    }
+                    .buttonStyle(.borderless)
+                    
+                    Spacer()
+                }
+            }
+            
+            Text(errorMessage)
+                .foregroundStyle(.red)
+            
+            Spacer()
+            Spacer()
+        }
     }
 }
+
+// MARK: - Methods
+extension LoginView {
+    private func login() async {
+        do {
+            let result = try await Auth.auth().signIn(withEmail: email, password: password)
+                // go to the main screen
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+    }
+}
+
 
 #Preview {
     LoginView()
