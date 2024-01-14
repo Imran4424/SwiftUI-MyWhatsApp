904fa07bc23fe975e53446a6f309cd78b23f1c0c
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2024-01-14 22:03:24 +0600

    add lisener for chats

--- MyWhatsApp/Model/ChatMessage.swift
+++ MyWhatsApp/Model/ChatMessage.swift
@@ -5,6 +5,7 @@
 //  Created by Shah Md Imran Hossain on 14/1/24.
 //
 
+import FirebaseFirestore
 import Foundation
 
 struct ChatMessage: Codable, Identifiable, Equatable {
@@ -27,5 +28,19 @@ extension ChatMessage {
             "dateCreated": dateCreated,
             "displayName": displayName
         ]
+    }
+    
+    static func fromSnapshot(snapshot: QueryDocumentSnapshot) -> ChatMessage? {
+        let dictionary = snapshot.data()
+        guard let text = dictionary["text"] as? String,
+              let uid = dictionary["uid"] as? String,
+              let dateCreated = (dictionary["dateCreated"] as? Timestamp)?.dateValue(),
+              let displayName = dictionary["displayName"] as? String else {
+            
+            print("snapshot's one or more data invalid")
+            return nil
+        }
+     
+        return ChatMessage(documentId: snapshot.documentID, text: text, uid: uid, dateCreated: dateCreated, displayName: displayName)
     }
 }
--- MyWhatsApp/Model/Model.swift
+++ MyWhatsApp/Model/Model.swift
@@ -13,11 +13,53 @@ import Foundation
 @MainActor
 class Model: ObservableObject {
     @Published var groups: [Group] = []
+    @Published var chatMessages: [ChatMessage] = []
+    
+    var firestoreListener: ListenerRegistration?
     
     func updateDisplayName(for user: User, displayName: String) async throws {
         let request = user.createProfileChangeRequest()
         request.displayName = displayName
         try? await request.commitChanges()
+    }
+    
+    func listenForChatMessages(in group: Group) {
+        chatMessages.removeAll()
+        
+        guard let groupDocumentId = group.documentId else {
+            print("group document id is nil")
+            return
+        }
+        
+        let db = Firestore.firestore()
+        
+        self.firestoreListener = db.collection("groups")
+            .document(groupDocumentId)
+            .collection("messages")
+            .order(by: "dateCreated", descending: false)
+            .addSnapshotListener({ [weak self] snapshot, error in
+                guard let snapshot else {
+                    print("error fetching snapshots: \(String(describing: error?.localizedDescription))")
+                    return
+                }
+                
+                snapshot.documentChanges.forEach { diff in
+                    if diff.type == .added {
+                        let chatMessage = ChatMessage.fromSnapshot(snapshot: diff.document)
+                        
+                        if let chatMessage {
+                            let exists = self?.chatMessages.contains(where: { cm in
+                                cm.documentId == chatMessage.documentId
+                            })
+                            
+                            if !(exists!) {
+                                self?.chatMessages.append(chatMessage)
+                            }
+                        }
+                    }
+                }
+            })
+        
     }
     
     func populateGroups() async throws {
